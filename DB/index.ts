bbab08d0e17c1660b176180b2e7a1822d974a2ce
ericgerzen <ericyes81007@gmail.com>
2025-06-04 13:37:52 -0300

    index created

---
+++ DB/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello from TechMarketBA-Back!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
