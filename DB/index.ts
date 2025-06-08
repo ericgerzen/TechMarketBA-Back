0726d1d3dd27a908421096ec499a0f9dfe405628
ericgerzen <ericyes81007@gmail.com>
2025-06-08 16:54:44 -0300

    users route is up

--- DB/index.ts
+++ DB/index.ts
@@ -4,12 +4,16 @@ import cors from 'cors';
 const app = express();
 const PORT = 3000;
 
+import usersRouter from './routes/users.router';
+
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('1');
 });
+
+app.use('/users', usersRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
