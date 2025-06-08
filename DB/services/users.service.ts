043ab56eaa6ce0e68f17b69db5376cd5eb87b247
ericgerzen <ericyes81007@gmail.com>
2025-06-08 16:50:13 -0300

    feat: using user.model.ts

--- DB/services/users.service.ts
+++ DB/services/users.service.ts
@@ -1,27 +1,41 @@
 import { pool } from "../db.ts";
+import { User } from "../models/user.model.ts";
 
-const getAllUsers = async () => {
+const getAllUsers = async ():Promise<User[]> => {
     const { rows } = await pool.query('SELECT * FROM users');
     return rows;
 };
 
-const getUserById = async (id_user:number) => {
+const getUserById = async (id_user:number) :Promise<User | null> => {
     const { rows } = await pool.query("SELECT * FROM users WHERE id_user = $1", [id_user]);
-    return rows[0];
+    return rows[0] || null;
 };
 
-const getUserByEmail = async (email:string) => {
+const getUserByEmail = async (email:string): Promise<User | null> => {
     const { rows } = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+    return rows[0] || null;
+};
+
+const createUser = async (
+    name: string,
+    surname: string,
+    email: string,
+    password: string
+): Promise<User> => {
+    const { rows } = await pool.query(
+        "INSERT INTO users (name, surname, email, password, seller, admin) VALUES ($1, $2, $3, $4, false, false) RETURNING *",
+        [name, surname, email, password]
+    );
     return rows[0];
 };
 
-const createUser = async (name:string, surname:string, email:string, password:string | undefined) => {
-    const { rows } = await pool.query("INSERT INTO users (name, surname, email, password, seller, admin) VALUES ($1, $2, $3, $4, false, false) RETURNING *",
-        [name, surname, email, password]);
-    return rows[0];
-};
-
-const updateUser = async (id_user:number, name:string, surname:string, email:string, password:string | undefined) => {
+const updateUser = async (
+    id_user: number,
+    name?: string,
+    surname?: string,
+    email?: string,
+    password?: string
+): Promise<User> => {
     const fields = [];
     const values = [];
     let query = "UPDATE users SET ";
@@ -48,24 +62,24 @@ const updateUser = async (id_user:number, name:string, surname:string, email:str
     }
 
     query += fields.join(", ");
-    query += ` WHERE id_user = $${fields.length + 1}`;
+    query += ` WHERE id_user = $${fields.length + 1} RETURNING *`;
     values.push(id_user);
 
     const { rows } = await pool.query(query, values);
     return rows[0];
 };
 
-const promoteUser = async (id_user:number) => {
+const promoteUser = async (id_user: number): Promise<number> => {
     await pool.query("UPDATE users SET seller = true WHERE id_user = $1", [id_user]);
     return id_user;
 };
 
-const crownUser = async (id_user:number) => {
+const crownUser = async (id_user: number): Promise<number> => {
     await pool.query("UPDATE users SET admin = true WHERE id_user = $1", [id_user]);
     return id_user;
-}
+};
 
-const deleteUser = async (id_user:number) => {
+const deleteUser = async (id_user: number): Promise<number> => {
     await pool.query("DELETE FROM users WHERE id_user = $1", [id_user]);
     return id_user;
 };
